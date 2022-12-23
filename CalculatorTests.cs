0bf4a8f7663f256e96479f974d7c474d58f2f4fc
Set-real <111229554+Set-real@users.noreply.github.com>
2022-12-23 13:23:25 +0300

    Add files via upload

---
+++ CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UserTicketService.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void MultiplicationMustReturnNotNullVallue()
+        {
+            var calculator = new Calculator();
+            Assert.Equal(8, calculator.Multiplication(2, 4));
+        }
+    }
+}
